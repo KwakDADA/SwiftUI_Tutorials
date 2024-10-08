b8fe3334c64b2b324f1e999c5f829a05dabbc973
KwakDADA <down022@gmail.com>
2024-10-08 13:46:51 +0900

    Section 3 - Combining views using stacks

--- 01_Creating_and_combining_views/MyWork/Landmarks/Landmarks/ContentView.swift
+++ 01_Creating_and_combining_views/MyWork/Landmarks/Landmarks/ContentView.swift
@@ -9,8 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Turtle Rock")
-            .font(.title)
+        VStack(alignment: .leading) {
+            Text("Turtle Rock")
+                .font(.title)
+            HStack {
+                Text("Joshua Tree National Park")
+                    .font(.subheadline)
+                Spacer() // to direct the layout to use the full width of the device
+                Text("California")
+                    .font(.subheadline)
+            }
+        }
+        .padding() // to give the landmark's name and details a little more space around their outer edges
     }
 }
 
