7619c45251c5d4cc11c13ceeb24022b160292a89
KwakDADA <down022@gmail.com>
2024-10-08 11:28:11 +0900

    Section 1 - Create a new project and explore the canvas

---
+++ 01_Creating_and_combining_views/MyWork/Landmarks/Landmarks/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by 곽다은 on 10/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello SwiftUI!")
+    }
+}
+
+#Preview {
+    ContentView()
+}
