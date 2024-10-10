6fbdd1e2ed85ed5f90d613e8efc868396e77936c
KwakDADA <down022@gmail.com>
2024-10-10 13:48:16 +0900

    Section 3 - Customize the row preview

--- 02_Building_lists_and_navigation/MyWork/Landmarks/Landmarks/Views/LandmarkRow.swift
+++ 02_Building_lists_and_navigation/MyWork/Landmarks/Landmarks/Views/LandmarkRow.swift
@@ -23,6 +23,21 @@ struct LandmarkRow: View {
     }
 }
 
+// Give each preview a name to help distinguish between them.
+//#Preview("Turtle Rock") {
+//    LandmarkRow(landmark: landmarks[0])
+//}
+//
+//#Preview("Salmon") {
+//    LandmarkRow(landmark: landmarks[1])
+//}
+
+// preview different versions of a view side by side
 #Preview {
-    LandmarkRow(landmark: landmarks[0])
+    // Group is a container for grouping view content.
+    // Xcode renders the group’s child views stacked as one preview in the canvas.
+    Group {
+        LandmarkRow(landmark: landmarks[0])
+        LandmarkRow(landmark: landmarks[1])
+    }
 }
