d11ea8f33e8c1ec1d32ef7b47cd75ea8ed539150
KwakDADA <down022@gmail.com>
2024-11-27 15:59:05 +0900

    Section 2 - Filter the list view

--- 03_Handling_user_input/MyWork/Landmarks/Landmarks/Views/LandmarkList.swift
+++ 03_Handling_user_input/MyWork/Landmarks/Landmarks/Views/LandmarkList.swift
@@ -8,9 +8,19 @@
 import SwiftUI
 
 struct LandmarkList: View {
+    // State is a value, or a set of values, that can change over time,
+    // and that affects a view’s behavior, content, or layout.
+    @State private var showFavoritesOnly = true
+    
+    var filteredLandmarks: [Landmark] {
+        landmarks.filter { landmark in
+            (!showFavoritesOnly || landmark.isFavorite)
+        }
+    }
+    
     var body: some View {
         NavigationSplitView {
-            List(landmarks) { landmark in
+            List(filteredLandmarks) { landmark in
                 NavigationLink {
                     LandmarkDetail(landmark: landmark)
                 } label: {
