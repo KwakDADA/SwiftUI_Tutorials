298b2b0616aafb5823f9a4a9933c424ad97e3485
KwakDADA <down022@gmail.com>
2024-12-06 16:16:37 +0900

    Working with UI controls - Section 2. Add an edit mode

--- Landmarks/Landmarks/Views/Profiles/ProfileHost.swift
+++ Landmarks/Landmarks/Views/Profiles/ProfileHost.swift
@@ -8,16 +8,29 @@
 import SwiftUI
 
 struct ProfileHost: View {
+    @Environment(\.editMode) var editMode
+    @Environment(ModelData.self) var modelData
     @State private var draftProfile = Profile.default
     
     var body: some View {
         VStack(alignment: .leading, spacing: 20) {
-            ProfileSummary(profile: draftProfile)
+            HStack {
+                Spacer()
+                EditButton()
+            }
+            
+            if editMode?.wrappedValue == .inactive {
+                ProfileSummary(profile: modelData.profile)
+            } else {
+                Text("Profile Editor")
+            }
         }
         .padding()
     }
 }
 
 #Preview {
+    // Even though this view doesn’t use a property with the @Environment property wrapper, ProfileSummary, a child of this view, does. So without the modifier, the preview fails.
     ProfileHost()
+        .environment(ModelData())
 }
