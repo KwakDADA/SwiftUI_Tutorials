8ab42d296bf52b733c44e8f6592e56b3632c17e3
KwakDADA <down022@gmail.com>
2024-12-17 18:25:47 +0900

    Creating a watchOS app - Section 5. Create a custom notification interface

--- Landmarks/WatchLandmark Watch App/ContentView.swift
+++ Landmarks/WatchLandmark Watch App/ContentView.swift
@@ -6,10 +6,19 @@
 //
 
 import SwiftUI
+import UserNotifications
 
 struct ContentView: View {
     var body: some View {
         LandmarkList()
+            // SwiftUI calls this asynchronous task modifier when the content view first appears.
+            .task {
+                // request authorization to enable notifications from Notification Center.
+                let center = UNUserNotificationCenter.current()
+                _ = try? await center.requestAuthorization(
+                    options: [.alert, .sound, .badge]
+                )
+            }
     }
 }
 
