e358767733ef9b8f177d107ec4d1fd9129c5481b
KwakDADA <down022@gmail.com>
2024-12-17 11:41:39 +0900

    Creating a watchOS app - Section 1. Add a watchOS target

---
+++ Landmarks/WatchLandmark Watch App/WatchLandmarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchLandmarkApp.swift
+//  WatchLandmark Watch App
+//
+//  Created by 곽다은 on 12/17/24.
+//
+
+import SwiftUI
+
+@main
+struct WatchLandmark_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
