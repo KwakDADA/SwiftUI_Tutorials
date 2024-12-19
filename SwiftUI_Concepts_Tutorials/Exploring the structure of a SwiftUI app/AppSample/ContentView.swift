1498aaa342126468fedaeb8f0df836f6536e6c40
KwakDADA <down022@gmail.com>
2024-12-19 15:47:10 +0900

    Exploring the structure of a SwiftUI app - Section 2. Content view

--- SwiftUI_Concepts_Tutorials/Exploring the structure of a SwiftUI app/AppSample/ContentView.swift
+++ SwiftUI_Concepts_Tutorials/Exploring the structure of a SwiftUI app/AppSample/ContentView.swift
@@ -4,7 +4,10 @@ See LICENSE folder for this sample’s licensing information.
 
 import SwiftUI
 
+// ContentView is a structure that conforms to the View protocol.
+// A view defines one or more visual elements that appear somewhere on a screen. A view is typically made up of other views, creating a view hierarchy.
 struct ContentView: View {
+    // ContentView implements the computed property body, just like the ‘MyApp’ structure does.
     var body: some View {
         VStack {
             Image(systemName: "globe")
