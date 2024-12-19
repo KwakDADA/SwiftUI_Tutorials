6b4f948f1f12115e335a35ce0fb5343d9718893e
KwakDADA <down022@gmail.com>
2024-12-19 17:16:00 +0900

    Specifying the view hierarchy of an app using a scene - Section 2. Define another view hierarchy

--- SwiftUI_Concepts_Tutorials/Specifying the view hierarchy of an app using a scene/SceneSample/MyApp.swift
+++ SwiftUI_Concepts_Tutorials/Specifying the view hierarchy of an app using a scene/SceneSample/MyApp.swift
@@ -8,6 +8,7 @@ import SwiftUI
 struct MyApp: App {
     // The computed body property can return one or more primary and secondary scenes.
     var body: some Scene {
+        // With two separate view hierarchies defined, the sample must specify which one to use based on the target platform.
         #if os(iOS)
         MyScene()
         #elseif os(macOS)
--- SwiftUI_Concepts_Tutorials/Specifying the view hierarchy of an app using a scene/SceneSample/Scenes/MyAlternativeScene.swift
+++ SwiftUI_Concepts_Tutorials/Specifying the view hierarchy of an app using a scene/SceneSample/Scenes/MyAlternativeScene.swift
@@ -11,7 +11,10 @@ struct MyAlternativeScene: Scene {
         }
         
         #if os(macOS)
+        // The sample uses the secondary scene Settings to provide a Settings menu item that’s available in the app menu, which is a common feature of Mac apps.
+        // The Settings scene is only available in macOS.
         Settings {
+            // The Settings scene contains the custom view, SettingView, which displays app settings in a window that the Settings menu item provides.
             SettingsView()
         }
         #endif
