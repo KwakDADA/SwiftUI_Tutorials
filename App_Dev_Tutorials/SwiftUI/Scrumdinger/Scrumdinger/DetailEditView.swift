c43788e44989ac384c3843ebed4fff92bbe064c3
KwakDADA <down022@gmail.com>
2025-01-14 17:35:45 +0900

    Creating the edit view

---
+++ App_Dev_Tutorials/SwiftUI/Scrumdinger/Scrumdinger/DetailEditView.swift
@@ -0,0 +1,61 @@
+//
+//  DetailEditView.swift
+//  Scrumdinger
+//
+//  Created by 곽다은 on 1/14/25.
+//
+
+import SwiftUI
+
+struct DetailEditView: View {
+    @State private var scrum = DailyScrum.emptyScrum
+    @State private var newAttendeeName = ""
+    
+    var body: some View {
+        Form {
+            Section {
+                TextField("Title", text: $scrum.title)
+                HStack {
+                    Slider(value: $scrum.lengthInMinutesAsDouble, in: 5...30, step: 1) {
+                        Text("Length")
+                    }
+                    .accessibilityLabel(Text("\(scrum.lengthInMinutes) minutes"))
+                    Spacer()
+                    Text("\(scrum.lengthInMinutes) minutes")
+                        .accessibilityHidden(true)
+                }
+            } header: {
+                Text("Meeting info")
+            }
+            Section {
+                ForEach(scrum.attendees) { attendee in
+                    Text(attendee.name)
+                }
+                .onDelete { indices in
+                    scrum.attendees.remove(atOffsets: indices)
+                }
+                HStack {
+                    TextField("New Attendee", text: $newAttendeeName)
+                    Button {
+                        withAnimation {
+                            let attendee = DailyScrum.Attendee(name: newAttendeeName)
+                            scrum.attendees.append(attendee)
+                            newAttendeeName = ""
+                        }
+                    } label: {
+                        Image(systemName: "plus.circle.fill")
+                            .accessibilityLabel("Add attendee")
+                    }
+                    .disabled(newAttendeeName.isEmpty)
+
+                }
+            } header: {
+                Text("Attendees")
+            }
+        }
+    }
+}
+
+#Preview {
+    DetailEditView()
+}
